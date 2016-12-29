6c7a2abca04a1c126255af6e8ad9a8b2f8f3d81c
Tobias Viehweger <tobias.viehweger@gmail.com>
2016-12-29 17:35:38 +0100

    Add support for AudioPlay.Play directive

---
+++ Alexa.NET/Response/Directive/AudioPlayerPlayDirective.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Alexa.NET.Response.Directive
+{
+    public class AudioPlayerDirective : IDirective
+    {
+        [JsonProperty("playBehavior")]
+        [JsonRequired]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public PlayBehavior PlayBehavior { get; set; }
+
+        [JsonProperty("audioItem")]
+        [JsonRequired]
+        public AudioItem AudioPlayerAudioItem { get; set; }
+
+        [JsonProperty("type")]
+        public string Type => "AudioPlayer.Play";
+    }
+}
