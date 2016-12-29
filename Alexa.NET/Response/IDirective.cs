d304947c6eac3ceda4622a832ffad033a92a81b7
Tobias Viehweger <tobias.viehweger@gmail.com>
2016-12-29 17:35:26 +0100

    Add support for Directives

---
+++ Alexa.NET/Response/IDirective.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alexa.NET.Response
+{
+    public interface IDirective
+    {
+        [JsonRequired]
+        string Type { get; }
+    }
+}
