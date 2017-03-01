323bde8fcecde1daf26a2d4511c3d6f38f4c1dc7
Tim Heuer <tim@timheure.com>
2017-03-01 13:24:36 -0800

    Fixed inheritance on Request.

--- Alexa.NET/Request/Type/AudioPlayerRequest.cs
+++ Alexa.NET/Request/Type/AudioPlayerRequest.cs
@@ -11,9 +11,6 @@ namespace Alexa.NET.Request.Type
         [JsonProperty("token")]
         public string Token { get; set; }
 
-        [JsonProperty("locale")]
-        public string Locale { get; set; }
-
         [JsonProperty("offsetInMilliseconds")]
         public string OffsetInMilliseconds { get; set; }
 
