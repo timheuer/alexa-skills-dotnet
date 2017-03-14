1b16904740c032fb4fa9509d1ccddabf7183929e
Matthias <matthias.shapiro@gmail.com>
2017-03-13 23:00:39 -0700

    Added PlayerController updates    - Added PlayerController intents and types    - Added System.ExceptionRequest intent    - Updated to support strong Error types    - Added StopDirective (for audio)    - Added ClearQueue and Stop response helpers to ResponseBuilder

---
+++ Alexa.NET/Request/Type/PlaybackControllerRequestType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Alexa.NET.Request.Type
+{
+    public enum PlaybackControllerRequestType
+    {
+        [EnumMember(Value = "NextCommandIssued")]
+        Next,
+        [EnumMember(Value = "PauseCommandIssued")]
+        Pause,
+        [EnumMember(Value = "PlayCommandIssued")]
+        Play,
+        [EnumMember(Value = "PreviousCommandIssued")]
+        Previous,
+        [EnumMember(Value = "Unknown")]
+        Unknown
+    }
+}
