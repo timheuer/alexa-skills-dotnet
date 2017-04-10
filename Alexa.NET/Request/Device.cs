2f083ebc5638c8fafc87ce4e6716b783a83ec9ed
Arne Stenmanns <stenmannsar@outlook.de>
2017-04-10 14:14:23 +0200

    Add Device location Api

--- Alexa.NET/Request/Device.cs
+++ Alexa.NET/Request/Device.cs
@@ -8,6 +8,9 @@ namespace Alexa.NET.Request
 {
     public class Device
     {
+        [JsonProperty("deviceId")]
+        public string DeviceID { get; set; }
+
         [JsonProperty("supportedInterfaces")]
         public Dictionary<string, object> SupportedInterfaces { get; set; }
 
---
+++ Alexa.NET/Request/Permissions.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Alexa.NET.Request
+{
+    public class Permissions
+    {
+        [JsonProperty("consentToken")]
+        public string ConsentToken { get; set; }
+    }
+}
