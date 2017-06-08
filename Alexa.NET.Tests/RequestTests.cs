01f082d31b398865b158d3ceae624ad412ccc28b
Steven Pears <steven.pears@live.co.uk>
2017-06-08 08:05:28 +0100

    Move across tests from the slight project Update them to match Alexa.NET Fix project type

---
+++ Alexa.NET.Tests/RequestTests.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Alexa.NET.Tests
+{
+    public class RequestTests
+    {
+        private string ExamplesPath = "Examples";
+
+        [Fact]
+        public void Can_read_IntentRequest_example()
+        {
+            const string example = "IntentRequest.json";
+            var json = File.ReadAllText(Path.Combine(ExamplesPath, example));
+            var convertedObj = JsonConvert.DeserializeObject<SkillRequest>(json);
+
+            Assert.NotNull(convertedObj);
+            Assert.Equal(typeof(IntentRequest), convertedObj.GetRequestType());
+        }
+
+        [Fact]
+        public void Can_read_LaunchRequest_example()
+        {
+            const string example = "LaunchRequest.json";
+            var json = File.ReadAllText(Path.Combine(ExamplesPath, example));
+            var convertedObj = JsonConvert.DeserializeObject<SkillRequest>(json);
+
+            Assert.NotNull(convertedObj);
+            Assert.Equal(typeof(LaunchRequest), convertedObj.GetRequestType());
+        }
+
+        [Fact]
+        public void Can_read_SessionEndedRequest_example()
+        {
+            const string example = "SessionEndedRequest.json";
+            var json = File.ReadAllText(Path.Combine(ExamplesPath, example));
+            var convertedObj = JsonConvert.DeserializeObject<SkillRequest>(json);
+
+            Assert.NotNull(convertedObj);
+            Assert.Equal(typeof(SessionEndedRequest), convertedObj.GetRequestType());
+        }
+
+        [Fact]
+        public void Can_read_slot_example()
+        {
+            const string example = "GetUtterance.json";
+            var json = File.ReadAllText(Path.Combine(ExamplesPath, example));
+            var convertedObj = JsonConvert.DeserializeObject<SkillRequest>(json);
+
+            var request = Assert.IsAssignableFrom<IntentRequest>(convertedObj.Request);
+            var slot = request.Intent.Slots["Utterance"];
+            Assert.Equal("how are you", slot.Value);
+        }
+
+        [Fact]
+        public void Can_accept_new_versions()
+        {
+            const string example = "SessionEndedRequest.json";
+            var json = File.ReadAllText(Path.Combine(ExamplesPath, example));
+            var convertedObj = JsonConvert.DeserializeObject<SkillRequest>(json);
+
+            Assert.NotNull(convertedObj);
+            Assert.Equal(typeof(SessionEndedRequest), convertedObj.GetRequestType());
+        }
+    }
+}
---
+++ Alexa.NET.Tests/ResponseTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Alexa.NET.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Xunit;
+
+namespace Alexa.NET.Tests
+{
+    public class ResponseTests
+    {
+        private const string ExamplesPath = @"Examples";
+
+        [Fact]
+        public void Should_create_same_json_response_as_example()
+        {
+            var skillResponse = new SkillResponse
+            {
+                Version = "1.0",
+                SessionAttributes = new Dictionary<string, object>
+                {
+                    {
+                        "supportedHoriscopePeriods", new
+                        {
+                            daily = true,
+                            weekly = false,
+                            monthly = false
+                        }
+                    }
+                },
+                Response = new ResponseBody
+                {
+                    OutputSpeech = new PlainTextOutputSpeech
+                    {
+                        Text =
+                            "Today will provide you a new learning opportunity. Stick with it and the possibilities will be endless. Can I help you with anything else?"
+                    },
+                    Card = new SimpleCard
+                    {
+                        Title = "Horoscope",
+                        Content =
+                            "Today will provide you a new learning opportunity. Stick with it and the possibilities will be endless."
+                    },
+                    ShouldEndSession = false
+                }
+            };
+
+            var json = JsonConvert.SerializeObject(skillResponse, Formatting.Indented, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+
+            const string example = "Response.json";
+            var workingJson = File.ReadAllText(Path.Combine(ExamplesPath, example));
+
+            workingJson = Regex.Replace(workingJson, @"\s", "");
+            json = Regex.Replace(json, @"\s", "");
+
+            Assert.Equal(workingJson, json);
+        }
+    }
+}
