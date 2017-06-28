29bf664cff498089325f62f7a8a0c802669f21d5
Steven Pears <github@lotsofideas.co.uk>
2017-06-28 08:39:21 +0100

    Add book Library intents

---
+++ Alexa.NET/Request/IntentLibrary/Books/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Alexa.NET.Request.IntentLibrary.Books
+{
+    public static class Book
+    {
+        public const string Add = "AMAZON.AddAction";
+        public const string Choose = "AMAZON.ChooseAction";
+        public const string Delete = "AMAZON.DeleteAction";
+        public const string Playback = "AMAZON.PlaybackAction";
+        public const string Rate = "AMAZON.RateAction";
+        public const string Restart = "AMAZON.RestartAction";
+        public const string Resume = "AMAZON.ResumeAction";
+        public const string Search = "AMAZON.SearchAction";
+        public const string Stop = "AMAZON.StopAction";
+        public const string Suspend = "AMAZON.SuspendAction";
+
+    }
+}
