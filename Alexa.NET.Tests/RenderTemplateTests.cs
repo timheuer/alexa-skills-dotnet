43eea2b177edfe11695360c309c541624edca280
Steven Pears <steven.pears@live.co.uk>
2017-07-01 17:47:14 +0100

    Add initial directive and test

---
+++ Alexa.NET.Tests/RenderTemplateTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Alexa.NET.Response.Directive;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Alexa.NET.Tests
+{
+    public class RenderTemplateTests
+    {
+        private const string ExamplesPath = "Examples";
+        //Examples found at: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/display-interface-reference#configure-your-skill-for-the-display-directive
+        
+        [Fact]
+        public void Creates_RenderTemplateDirective()
+        {
+            var actual = new DisplayRenderTemplateDirective();
+            Assert.True(CompareJson(actual,"DisplayRenderTemplateDirective.json"));
+        }
+
+        private bool CompareJson(object actual, string expectedFile)
+        {
+
+            var actualJObject = JObject.FromObject(actual);
+            var expected = File.ReadAllText(Path.Combine(ExamplesPath, expectedFile));
+            var expectedJObject = JObject.Parse(expected);
+            Console.WriteLine(actualJObject);
+            return JToken.DeepEquals(expectedJObject, actualJObject);
+        }
+    }
+}
---
+++ Alexa.NET/Response/Directive/DisplayRenderTemplateDirective.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Alexa.NET.Response.Directive
+{
+    public class DisplayRenderTemplateDirective:IDirective
+    {
+        [JsonProperty("type")]
+        public string Type => "Display.RenderTemplate";
+    }
+}
