84f1077862cf1b0b6fb604c262958f8d530dabda
Steven Pears <steven.pears@experian.com>
2017-11-14 16:19:11 +0000

    Test request object being created

--- Alexa.NET.Tests/ProgressiveResponseTests.cs
+++ Alexa.NET.Tests/ProgressiveResponseTests.cs
@@ -31,23 +31,59 @@ namespace Alexa.NET.Tests
         }
 
         [Fact]
+        public async Task ProgressiveResponseCallsBaseUrl()
+        {
+            var passed = false;
+
+            var response = CreateProgressiveResponse(req =>
+            {
+                passed = req.RequestUri.Host == "localhost";
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            });
+
+            await response.SendSpeech("hello");
+
+            Assert.True(passed);
+        }
+
+        [Fact]
         public async Task ProgressiveResponseCallsDirectiveApi()
         {
             var passed = false;
 
-            var handlerAction = new Func<HttpRequestMessage, HttpResponseMessage>(req =>
+            var response = CreateProgressiveResponse(req =>
             {
                 passed = req.RequestUri.AbsolutePath == "/v1/directives";
                 return new HttpResponseMessage(HttpStatusCode.NoContent);
             });
 
-            var handler = new ActionMessageHandler(handlerAction);
-            var client = new HttpClient(handler) {BaseAddress = new Uri("https://localhost")};
-
-            var response = new ProgressiveResponse("xxx", "authToken", "http://localhost", client);
             await response.SendSpeech("hello");
 
             Assert.True(passed);
+        }
+
+        [Fact]
+        public async Task ProgressiveResponseSetsAuthorizationHandler()
+        {
+            var passed = false;
+
+            var response = CreateProgressiveResponse(req =>
+            {
+                passed = req.Headers.Authorization.Scheme == "Bearer";
+                passed = req.Headers.Authorization.Parameter == "authToken";
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            });
+
+            await response.SendSpeech("hello");
+
+            Assert.True(passed);
+        }
+
+        private ProgressiveResponse CreateProgressiveResponse(Func<HttpRequestMessage, HttpResponseMessage> handlerAction)
+        {
+            var handler = new ActionMessageHandler(handlerAction);
+            var client =  new HttpClient(handler);
+            return new ProgressiveResponse("xxx", "authToken", "http://localhost", client);
         }
     }
 
