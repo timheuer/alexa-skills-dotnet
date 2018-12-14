a4a2594553315940512fcdf82d16d7dbdeb7b115
Steven Pears <steven.pears@live.co.uk>
2018-12-14 08:45:45 +0000

    Move geolocation to its own file

--- Alexa.NET/Request/Context.cs
+++ Alexa.NET/Request/Context.cs
@@ -1,6 +1,5 @@
 ﻿using Alexa.NET.Request.Type;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,21 +16,5 @@ namespace Alexa.NET.Request
 
         [JsonProperty("Geolocation")]
         public Geolocation Geolocation { get; set; }
-    }
-
-    public class Geolocation
-    {
-        [JsonProperty("locationServices", NullValueHandling = NullValueHandling.Ignore)]
-        public LocationServices LocationServices { get; set; }
-        [JsonProperty("timestamp")]
-        public DateTimeOffset Timestamp { get; set; }
-        [JsonProperty("coordinate", NullValueHandling = NullValueHandling.Ignore)]
-        public GeolocationCoordinate Coordinate { get; set; }
-        [JsonProperty("altitude",NullValueHandling = NullValueHandling.Ignore)]
-        public GeolocationAltitude Altitude { get; set; }
-        [JsonProperty("heading", NullValueHandling = NullValueHandling.Ignore)]
-        public GeolocationHeading Heading { get; set; }
-        [JsonProperty("speed", NullValueHandling = NullValueHandling.Ignore)]
-        public GeolocationSpeed Speed { get; set; }
     }
 }
---
+++ Alexa.NET/Request/Geolocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Alexa.NET.Request.Type;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.Request
+{
+    public class Geolocation
+    {
+        [JsonProperty("locationServices", NullValueHandling = NullValueHandling.Ignore)]
+        public LocationServices LocationServices { get; set; }
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; set; }
+        [JsonProperty("coordinate", NullValueHandling = NullValueHandling.Ignore)]
+        public GeolocationCoordinate Coordinate { get; set; }
+        [JsonProperty("altitude",NullValueHandling = NullValueHandling.Ignore)]
+        public GeolocationAltitude Altitude { get; set; }
+        [JsonProperty("heading", NullValueHandling = NullValueHandling.Ignore)]
+        public GeolocationHeading Heading { get; set; }
+        [JsonProperty("speed", NullValueHandling = NullValueHandling.Ignore)]
+        public GeolocationSpeed Speed { get; set; }
+    }
+}
