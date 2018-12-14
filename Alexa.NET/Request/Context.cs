2d20b4ea73d4299b04f65ee1e13915642a4a153f
Steven Pears <steven.pears@live.co.uk>
2018-12-14 08:42:24 +0000

    Add geolocation support to Context Add test for Geolocation structure

--- Alexa.NET/Request/Context.cs
+++ Alexa.NET/Request/Context.cs
@@ -15,5 +15,23 @@ namespace Alexa.NET.Request
         [JsonProperty("AudioPlayer")]
         public PlaybackState AudioPlayer { get; set; }
 
+        [JsonProperty("Geolocation")]
+        public Geolocation Geolocation { get; set; }
+    }
+
+    public class Geolocation
+    {
+        [JsonProperty("locationServices", NullValueHandling = NullValueHandling.Ignore)]
+        public LocationServices LocationServices { get; set; }
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; set; }
+        [JsonProperty("coordinate", NullValueHandling = NullValueHandling.Ignore)]
+        public GeolocationCoordinate Coordinate { get; set; }
+        [JsonProperty("altitude",NullValueHandling = NullValueHandling.Ignore)]
+        public GeolocationAltitude Altitude { get; set; }
+        [JsonProperty("heading", NullValueHandling = NullValueHandling.Ignore)]
+        public GeolocationHeading Heading { get; set; }
+        [JsonProperty("speed", NullValueHandling = NullValueHandling.Ignore)]
+        public GeolocationSpeed Speed { get; set; }
     }
 }
---
+++ Alexa.NET/Request/Type/LocationServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.Request.Type
+{
+    public class LocationServices
+    {
+        [JsonProperty("access")]
+        public LocationServiceAccess Access { get; set; }
+
+        [JsonProperty("status")]
+        public LocationServiceStatus Status { get; set; }
+    }
+}
