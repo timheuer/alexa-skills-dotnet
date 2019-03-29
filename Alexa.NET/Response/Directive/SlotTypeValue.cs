ae1f2ab0beac8984c2d62382037515586d1ef105
Steven Pears <steven.pears@live.co.uk>
2019-03-29 23:19:00 +0000

    Add directive, slot types and test

---
+++ Alexa.NET/Response/Directive/SlotTypeValue.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Alexa.NET.Response.Directive
+{
+    public class SlotTypeValue
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public SlotTypeValueName Name { get; set; }
+    }
+}
