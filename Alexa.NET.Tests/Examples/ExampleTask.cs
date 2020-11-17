61fb42e5d6dc4501491bb5ce8d4d249009c5838e
Aadu Pirn <pirnaadu@gmail.com>
2020-11-17 13:41:33 -0500

    Add custom task converter test

---
+++ Alexa.NET.Tests/Examples/ExampleTask.cs
@@ -0,0 +1,12 @@
+﻿using Alexa.NET.ConnectionTasks;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.Tests.Examples
+{
+    public class ExampleTask : IConnectionTask
+    {
+        public string ConnectionUri { get; set; }
+        [JsonProperty("randomParameter")]
+        public string RandomParameter { get; set; }
+    }
+}
---
+++ Alexa.NET.Tests/Examples/ExampleTaskConverter.cs
@@ -0,0 +1,31 @@
+﻿using Alexa.NET.ConnectionTasks;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response.Converters;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace Alexa.NET.Tests.Examples
+{
+    public class ExampleTaskConverter : IConnectionTaskConverter
+    {
+        public bool CanConvert(JObject jObject)
+        {
+            var property = jObject["randomParameter"];
+            return property is object;
+        }
+
+        public IConnectionTask Convert(JObject jObject)
+        {
+            return new ExampleTask();
+        }
+
+        public static void AddToConnectionTaskConverters()
+        {
+            if (ConnectionTaskConverter.ConnectionTaskConverters.Where(rc => rc != null)
+                .All(rc => rc.GetType() != typeof(ExampleTaskConverter)))
+            {
+                ConnectionTaskConverter.ConnectionTaskConverters.Add(new ExampleTaskConverter());
+            }
+        }
+    }
+}
