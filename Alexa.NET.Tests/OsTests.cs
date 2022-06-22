e1f6521f7f043cc1632baa062922545b4283706c
Tim Heuer <tim@timheuer.com>
2022-06-21 20:02:48 -0700

    OS testing example and remote env

---
+++ Alexa.NET.Tests/OsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alexa.NET.Tests
+{
+    public class OsTests
+    {
+        [Fact]
+        public void Ensure_Runs_On_Windows()
+        {
+            Assert.True(RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
+        }
+
+        [Fact]
+        public void Ensure_Runs_On_Linux()
+        {
+            Assert.True(RuntimeInformation.IsOSPlatform(OSPlatform.Linux));
+        }
+    }
+}
